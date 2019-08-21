4f02db37b544fb474e84f221bb65722dce615199
Nick Konecny <nkonecny@stashinvest.com>
2019-08-21 16:54:51 -0400

    updoot

--- main/animations.h
+++ main/animations.h
@@ -2,19 +2,17 @@ void makeBurningManAnimation(Adafruit_NeoMatrix * matrix) {
   drawBM(matrix);
   delay(5000);
   colorWipeBottomUp(matrix, matrix->Color( 226, 88, 34), 50);
-  delay(5000);    
+  delay(5000);
   colorWipe(matrix, matrix->Color( 0, 0, 0), 10);
   delay(5000);
 }
 
 
 void makeFireAnimation(Adafruit_NeoMatrix * matrix) {
-  for (int i = 0; i < 20; i++) {
-    drawFire1(matrix);
-    delay(50);
-    drawFire3(matrix);
-    delay(50);
-    drawFire2(matrix);
-    delay(50);    
+  int length = sizeof(fireImages);
+  for (int repeat = 0; repeat < 20; repeat++) {
+    for (int i = 0; i < length; i++) {
+      displayMatrixImage(matrix, fireImages[i]);
+    }
   }
 }
