7e3b60f9df0ec6a776ebb398a92a274325357311
Slackcat <slackcat@stash.com>
2022-08-14 15:01:54 -0400

    updoot

--- main/main.ino
+++ main/main.ino
@@ -2,52 +2,28 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_NeoMatrix.h>
 #include <Adafruit_NeoPixel.h>
+#include "shared/animations.h"
 
 #define PIN D8
 #define BRIGHTNESS 5
 #define mw 44
 #define mh 11
 
-
-Adafruit_NeoMatrix *matrix = new Adafruit_NeoMatrix(mw, mh, PIN,
-    NEO_MATRIX_TOP     + NEO_MATRIX_LEFT +
-    NEO_MATRIX_ROWS + NEO_MATRIX_ZIGZAG,
-    NEO_GRB            + NEO_KHZ800);
+uint8_t matrixType = NEO_MATRIX_TOP + NEO_MATRIX_LEFT + NEO_MATRIX_ROWS + NEO_MATRIX_ZIGZAG;
+Adafruit_NeoMatrix *matrix = new Adafruit_NeoMatrix(mw, mh, PIN, matrixType, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(mw * mh, PIN, NEO_GRB + NEO_KHZ800);
 
 
-Adafruit_NeoMatrix *textMatrix = new Adafruit_NeoMatrix(mw, mh, PIN,
-    NEO_MATRIX_TOP     + NEO_MATRIX_LEFT +
-    NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
-    NEO_GRB            + NEO_KHZ800);
-
-
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(128, PIN, NEO_GRB + NEO_KHZ800);
-
-#include "animations.h"
 void setup() {
   Serial.begin(115200);
-//  matrix->begin();
-//  matrix->setTextWrap(false);
-//  matrix->setBrightness(BRIGHTNESS);
-//  matrix->clear();
-
-  textMatrix->begin();
-  textMatrix->setTextWrap(false);
-  textMatrix->setBrightness(BRIGHTNESS);
-  textMatrix->clear();
-  
-  strip.begin(); // Initialize NeoPixel strip object (REQUIRED)
-  strip.show();  // Initialize all pixels to 'off'
-  strip.setBrightness(BRIGHTNESS);
-
-  //Flips em!
-//  initImages();
-
-  
+  matrix->begin();
+  matrix->setTextWrap(false);
+  matrix->setBrightness(BRIGHTNESS);
+  matrix->clear();
 }
 
 
 void loop() {
-  strip.clear();
-  startBm2019();
+  matrix->clear();
+  start(matrix);
 }
---
+++ main/shared/text.h
@@ -0,0 +1,65 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoMatrix.h>
+#include <Adafruit_NeoPixel.h>
+
+int CENTER_TEXT_CURSOR_OFFSET = 2;
+
+void initScrollTextTopToBottom(Adafruit_NeoMatrix *matrix, int color) {
+  matrix->clear();
+  matrix->setTextWrap(false);
+  matrix->setTextSize(1);
+  matrix->setRotation(0);
+}
+
+void displayText(Adafruit_NeoMatrix *matrix, int index, String textToDisplay, int color) {
+  matrix->clear();   
+  matrix->setCursor(index, CENTER_TEXT_CURSOR_OFFSET);
+  matrix->print(textToDisplay);
+  matrix->setTextColor(color);
+  matrix->show();
+}
+
+
+void scrollText(Adafruit_NeoMatrix *matrix, String textToDisplay, int color) {
+  int screenWidth = matrix->width();
+  int scrollLength = textToDisplay.length() + (screenWidth * 1.5);
+  
+  initScrollTextTopToBottom(matrix, color);
+  
+  for (int8_t index = scrollLength; index > scrollLength * -1 ; index--) {
+    displayText(matrix, index, textToDisplay, color); 
+    delay(50);
+  }  
+
+  matrix->clear();
+}
+
+
+// void clearTop() {
+//   for (int row = 0; row < 8; row++) {
+//     for (int column = 0; column < 8; column++) {
+//       matrix->drawPixel(row, column, strip.Color(0, 0, 0));
+//     }
+//   }
+// }
+
+// void scrollTextNoClear(String textToDisplay, MatrixImage img, int color) {
+//   int x = matrix->width();
+
+//   // Account for 6 pixel wide characters plus a space
+//   int pixelsInText = textToDisplay.length() * mh;
+//   matrix->setTextSize(1);
+//   matrix->setRotation(3);
+//   matrix->setTextColor(color);
+//   matrix->setCursor(x, 0);
+//   matrix->print(textToDisplay);
+//   matrix->show();
+
+//   displayMatrix(img);
+//   while (x > (matrix->width() - pixelsInText)) {
+//     matrix->setCursor(--x, 0);
+//     matrix->print(textToDisplay);
+//     matrix->show();
+//     delay(150);
+//   }
+// }
