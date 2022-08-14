f358616796654f216821f1ddfc56d99110bc1111
Slackcat <slackcat@stash.com>
2022-08-14 15:16:52 -0400

    updoot

--- main/animations.h
+++ main/animations.h
@@ -1,65 +1,64 @@
-#include "shit.h"
-#include "color.h"
-#include "util.h"
-#include "images.h"
+// #include "shit.h"
+// #include "color.h"
+// #include "util.h"
+// #include "images.h"
 #include "text.h"
 
 
-void cleanup() {
-  strip.clear();
-  matrix->clear();
-}
+// void cleanup() {
+//   matrix->clear();
+// }
 
-void makeFireAnimation() {
-  for (int repeat = 0; repeat < 60; repeat++) {
-    for (int i = 0; i < 3; i++) {
-      displayMatrix(fireImages[i]);
-    }
-    for (int i = 2; i >= 0; i--) {
-      displayMatrix(fireImages[i]);
-    }
-  }
-}
+// void makeFireAnimation() {
+//   for (int repeat = 0; repeat < 60; repeat++) {
+//     for (int i = 0; i < 3; i++) {
+//       displayMatrix(fireImages[i]);
+//     }
+//     for (int i = 2; i >= 0; i--) {
+//       displayMatrix(fireImages[i]);
+//     }
+//   }
+// }
 
-void pingpongImage(MatrixImage img, int loops, int (*decayFn)(int)) {
-  while (loops-- > 0) {
-    for (int i = 8; i >= 0; i--) {
-      displayMatrix(img);
-      shiftUpArray(img.image);            
-      delay((*decayFn)(loops));
-    }
+// void pingpongImage(MatrixImage img, int loops, int (*decayFn)(int)) {
+//   while (loops-- > 0) {
+//     for (int i = 8; i >= 0; i--) {
+//       displayMatrix(img);
+//       shiftUpArray(img.image);            
+//       delay((*decayFn)(loops));
+//     }
 
-    for (int i = 8; i >= 0; i--) {
-      displayMatrix(img);
-      shiftDownArray(img.image);
-      delay((*decayFn)(loops));
-    }
-  }
-}
+//     for (int i = 8; i >= 0; i--) {
+//       displayMatrix(img);
+//       shiftDownArray(img.image);
+//       delay((*decayFn)(loops));
+//     }
+//   }
+// }
 
-int decay(int loops) {
-  return 16 + (16 * loops);
-}
+// int decay(int loops) {
+//   return 16 + (16 * loops);
+// }
 
-int noDecay(int loops) {
-  return 16;
-}
+// int noDecay(int loops) {
+//   return 16;
+// }
  
-void panImage(MatrixImage mtrx) {
-  for (int i = 100; i >= 0; i--) {
-    displayMatrix(mtrx);
-    shiftLoopArray(mtrx.image);
-    delay(16 * 5);
-  }
-}
+// void panImage(MatrixImage mtrx) {
+//   for (int i = 100; i >= 0; i--) {
+//     displayMatrix(mtrx);
+//     shiftLoopArray(mtrx.image);
+//     delay(16 * 5);
+//   }
+// }
 
-void startBm2019() {
+void startBm2019(Adafruit_NeoMatrix *matrix) {
   
   // pingpongImage(eggplant_animate, 10, decay);
   // pingpongImage(eggplant_animate, 15, noDecay);
   // cleanup();
   
-  scrollText("FUCK YOUR BURN", textMatrix->Color(255, 0, 255));
+  scrollText(matrix, "FUCK YOUR BURN", matrix->Color(255, 0, 255));
 //  display_resolution();
 
 
--- main/text.h
+++ main/text.h
@@ -4,68 +4,62 @@
 
 int CENTER_TEXT_CURSOR_OFFSET = 2;
 
-void scrollText(String textToDisplay, int color) {
-//  textMatrix->clear();
-//  textMatrix->setTextWrap(false);  // we don't wrap text so it scrolls nicely
-//  textMatrix->setTextSize(1);
-//  textMatrix->setRotation(0);
-//  
-  int screenWidth = matrix->width();
-  int scrollLength = textToDisplay.length() + (screenWidth * 1.5);
-  
-
-
+void initScrollTextTopToBottom(Adafruit_NeoMatrix *matrix, int color) {
   matrix->clear();
   matrix->setTextWrap(false);
   matrix->setTextSize(1);
   matrix->setRotation(0);
+}
+
+void displayText(Adafruit_NeoMatrix *matrix, int index, String textToDisplay, int color) {
+  matrix->clear();   
+  matrix->setCursor(index, CENTER_TEXT_CURSOR_OFFSET);
+  matrix->print(textToDisplay);
   matrix->setTextColor(color);
-
-  
-
-  for (int8_t index = scrollLength; index > scrollLength * -1 ; index--) {
-    Serial.println(index);
-    Serial.println(scrollLength);
-    matrix->clear();
-    matrix->setCursor(index, CENTER_TEXT_CURSOR_OFFSET);
-    matrix->print(textToDisplay);
-    matrix->show();
-    delay(50);
-  }
-
-  
-  matrix->setCursor(0, 0);
   matrix->show();
 }
 
 
-void clearTop() {
-  for (int row = 0; row < 8; row++) {
-    for (int column = 0; column < 8; column++) {
-      textMatrix->drawPixel(row, column, strip.Color(0, 0, 0));
-    }
-  }
+void scrollText(Adafruit_NeoMatrix *matrix, String textToDisplay, int color) {
+  int screenWidth = matrix->width();
+  int scrollLength = textToDisplay.length() + (screenWidth * 1.5);
+  
+  initScrollTextTopToBottom(matrix, color);
+  
+  for (int8_t index = scrollLength; index > scrollLength * -1 ; index--) {
+    displayText(matrix, index, textToDisplay, color); 
+    delay(50);
+  }  
+
+  matrix->clear();
 }
 
-void scrollTextNoClear(String textToDisplay, MatrixImage img, int color) {
-  int x = matrix->width();
 
-  // Account for 6 pixel wide characters plus a space
-  int pixelsInText = textToDisplay.length() * mh;
-  textMatrix->setTextSize(1);
-  textMatrix->setRotation(3);
-  textMatrix->setTextColor(color);
-  textMatrix->setCursor(x, 0);
-  textMatrix->print(textToDisplay);
-  textMatrix->show();
+// void clearTop() {
+//   for (int row = 0; row < 8; row++) {
+//     for (int column = 0; column < 8; column++) {
+//       matrix->drawPixel(row, column, strip.Color(0, 0, 0));
+//     }
+//   }
+// }
 
-  displayMatrix(img);
-  while (x > (matrix->width() - pixelsInText)) {
-    //textMatrix->fillScreen(matrix->Color(0, 0, 0));
-    //clearTop();
-    textMatrix->setCursor(--x, 0);
-    textMatrix->print(textToDisplay);
-    textMatrix->show();
-    delay(150);
-  }
-}
+// void scrollTextNoClear(String textToDisplay, MatrixImage img, int color) {
+//   int x = matrix->width();
+
+//   // Account for 6 pixel wide characters plus a space
+//   int pixelsInText = textToDisplay.length() * mh;
+//   matrix->setTextSize(1);
+//   matrix->setRotation(3);
+//   matrix->setTextColor(color);
+//   matrix->setCursor(x, 0);
+//   matrix->print(textToDisplay);
+//   matrix->show();
+
+//   displayMatrix(img);
+//   while (x > (matrix->width() - pixelsInText)) {
+//     matrix->setCursor(--x, 0);
+//     matrix->print(textToDisplay);
+//     matrix->show();
+//     delay(150);
+//   }
+// }
