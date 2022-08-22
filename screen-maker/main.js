b71f2c552f762b2a1ecbd045eb7636f0849d5c05
Slackcat <slackcat@stash.com>
2022-08-22 19:16:34 -0400

    updoot

--- screen-maker/main.js
+++ screen-maker/main.js
@@ -11,6 +11,13 @@ const colorsDiv = document.getElementById("colors")
 let globalColor = document.querySelector('.color').value
 let draw = false
 
+
+window.addEventListener('keydown', function (event) {
+  if (event.key === 'z') {
+
+  }
+});
+
 function populate(rows, columns) {
   container.style.setProperty('--columns', columns)
   container.style.setProperty('--rows', rows)
@@ -51,20 +58,31 @@ function reset() {
 }
 
 function exportJson() {
-  const defaultColor = "rgb(0,0,0)";
+  const defaultColor = "rgb(61, 61, 61)";
   const matrix = [];
   let row = [];
+
+  const colors = [defaultColor];
+
   [...container.children].forEach((child, index) => {
-    if (index % 11) {
+    if (index > 0 && index % 11 === 0) {
       matrix.push(row)
       row = [];
     }
     const pixelColor = child.style.backgroundColor || defaultColor
-    row.push(pixelColor)
+    const colorIndex = colors.indexOf(pixelColor);
+    if (colorIndex === -1) {
+      colors.push(pixelColor)
+    }
+
+    row.push(colors.indexOf(pixelColor))
   })
 
-  const json = JSON.stringify(matrix);
-  console.log(json);
+  console.log(matrix)
+  const json = JSON.stringify({
+    colors,
+    pixels: matrix
+  });
   copy(json)
 }
 
@@ -82,23 +100,31 @@ function addColor(newColor) {
 }
 
 async function drawArray() {
-  const pixels = [...container.children];
-  const data = JSON.parse(textArea.value);
-  const flatArray = data.reduce((acc, val) => acc.concat(...val));
-  pixels.forEach((pixel, idx) => {
-    pixel.style.backgroundColor = flatArray[idx];
-  })
+  try {
+    const pixels = [...container.children];
+    const data = JSON.parse(textArea.value);
+    const flatArray = data.pixels.reduce((acc, val) => acc.concat(...val));
+    pixels.forEach((pixel, idx) => {
+      console.log(`Drawking pixel: ${idx} | color: ${data.colors[flatArray[idx]]}`)
+      pixel.style.backgroundColor = data.colors[flatArray[idx]];
+    })
+  } catch (e) {
+    console.warn(e);
+  }
+
 }
 
 
+addColor("rgb(61, 61, 61)")
 addColor("#FF0000")
 addColor("#00FF00")
 addColor("#0000FF")
+addColor("#FFFFFF")
 
 resetBtn.addEventListener('click', reset)
 exprotBtn.addEventListener('click', exportJson)
 drawBtn.addEventListener('click', drawArray)
-color.addEventListener('change', function(event) {
+color.addEventListener('change', function (event) {
   addColor(event.target.value)
 });
 populate(rows, columns)
